fb734601e373414945258ad59d4fcbc5afbb9df9
satheesh Soldat Inc <jskcbe@gmail.com>
2017-08-24 15:21:19 -0500

    Update docker-install.sh

--- docker-install.sh
+++ docker-install.sh
@@ -1,8 +1,8 @@
-curl -fsSL https://download.docker.com/linux/ubuntu/gpg | sudo apt-key add -
-sudo add-apt-repository "deb [arch=amd64] https://download.docker.com/linux/ubuntu $(lsb_release -cs) stable"
-sudo apt-get update
+curl -fsSL https://download.docker.com/linux/ubuntu/gpg | apt-key add -
+add-apt-repository "deb [arch=amd64] https://download.docker.com/linux/ubuntu $(lsb_release -cs) stable"
+apt-get update
 apt-cache policy docker-ce
-sudo apt-get install -y docker-ce
-sudo systemctl status docker
-sudo usermod -aG docker ${USER}
-sudo usermod -aG docker jenkins
+apt-get install -y docker-ce
+systemctl status docker
+usermod -aG docker ${USER}
+usermod -aG docker jenkins
